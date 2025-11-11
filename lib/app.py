8819b322a05d3988ff814f3a37944af0c6f924e6
Collins Kibet <kollcibe05@gmail.com>
2025-11-11 21:36:31 +0300

    printed inline and multiline logs

---
+++ lib/app.py
@@ -0,0 +1,13 @@
+#Do me dirty and learn
+
+print("Hello World! Pass this test, please.")
+
+# def foo (name):
+#     print("Well Hello Bigman") 
+
+# foo("Collins") 
+
+# print("\n")
+
+# print("We are", end="!! ")
+# print("who we are", end="\n and that is all that matters")
